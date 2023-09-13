cd8d9a6a9fda495423d96e119adc1a541e53ee7e
Anais Vaillant <anais.vaillant@fishtownanalytics.com>
2023-09-13 14:57:42 +0000

    change version

--- models/analytics/fct_revenue_summary.sql
+++ models/analytics/fct_revenue_summary.sql
@@ -1,5 +1,5 @@
 select
         year(order_date) as year,
         sum(net_item_sales_amount) as total_revenue
-from {{ ref('anais_sandbox', 'fct_orders') }}
+from {{ ref('anais_sandbox', 'fct_orders', v='1') }}
 group by 1
